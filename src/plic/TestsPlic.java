31bc78c5f829bffabc0d034a371d82edda9670ac
Simon Hajek <simon.hajek2@etu.univ-lorraine.fr>
2017-01-16 20:13:02 +0100

    Ajout de la classe TestsPlic permettant de tester tout les plics d'un coup

---
+++ src/plic/TestsPlic.java
@@ -0,0 +1,23 @@
+package plic;
+
+/**
+ * Created by simon on 16/01/17.
+ */
+public class TestsPlic {
+
+    /**
+     * Appel à Plic pour tester tous les plics
+     * @param i le plic Numéro tant
+     */
+    private static void plic(int i){
+        System.out.println("======================== PLIC NUMERO : "+i+" ===========================");
+        new Plic("res/test"+i+".plic");
+        System.out.println("======================== FIN PLIC "+i+" =========================");
+    }
+
+    public static void main(String[] args) {
+        for(int i  = 1; i<11; i++){
+            plic(i);
+        }
+    }
+}
