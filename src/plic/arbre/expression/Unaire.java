7efcbec9c81d957dd9d7c780b99006b794dd543f
Thomas Janowskyj <Thomas Janowskyj>
2017-02-13 23:59:37 +0100

    Quelques modifications, correction du plic0 ? Problème dans le JCUp

--- src/plic/arbre/expression/Unaire.java
+++ src/plic/arbre/expression/Unaire.java
@@ -27,7 +27,7 @@ public abstract class Unaire extends Expression {
                 strb.append("andi $v0, $v0, 0x1\n");
                 break;
             case "- ":
-                strb.append("neg $v0, $t8\n");
+                strb.append("neg $v0, $v0 \n");
                 break;
         }
         return strb.toString();
