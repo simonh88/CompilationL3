a9eeca4282135885bde9d01d5d342d698f4f3251
Simon Hajek <simon.hajek2@etu.univ-lorraine.fr>
2017-02-17 00:10:45 +0100

    Debug hmap

--- src/plic/tds/Entree.java
+++ src/plic/tds/Entree.java
@@ -18,4 +18,20 @@ public class Entree {
     public String toString(){
         return nom;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Entree entree = (Entree) o;
+
+        return nom != null ? nom.equals(entree.nom) : entree.nom == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return nom != null ? nom.hashCode() : 0;
+    }
 }
